d1c49fff288a217b86893b42d8d0450d97ab667b
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-03-13 23:09:41 -0400

    Finished Adding a model

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddDbContext<MoviesContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
